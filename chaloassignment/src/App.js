38fa538e27e7723f4f5aca0267fe85566d16c59c
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-14 22:43:16 +0530

    Rendering CreateForm component in App.js

--- chaloassignment/src/App.js
+++ chaloassignment/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateForm from './components/Form/CreateForm';
 
 function App() {
   return (
     <div className="App">
+      <CreateForm />
     </div>
   );
 }
