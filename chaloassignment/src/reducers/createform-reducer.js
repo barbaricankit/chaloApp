9faa6b3477f001f4dd7c9a87c19d3fb8bc4a1aa8
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-14 22:42:17 +0530

    Added ENTER_ROUTE_NAME type in createform reducer

--- chaloassignment/src/reducers/createform-reducer.js
+++ chaloassignment/src/reducers/createform-reducer.js
@@ -1,7 +1,8 @@
 export const manageState=(state,action)=>{
 
     switch(action.type){
-    
+        case "ENTER_ROUTE_NAME":
+            return {...state,routeName:action.payload.value}
         default :
         return state
     }
