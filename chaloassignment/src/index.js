017ae8f119f225a54e05a0ec90672b09e9dfa603
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-14 22:44:08 +0530

    Wrapping App.js with createform provider

--- chaloassignment/src/index.js
+++ chaloassignment/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CreateFormProvider } from './context/createform-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CreateFormProvider>
     <App />
+    </CreateFormProvider>
   </React.StrictMode>
 );
 
