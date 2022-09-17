f2942f6ea1922f9b9604a61e247e3f2d7be83336
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 18:16:58 +0530

    Create route context for handling all the routes data

---
+++ chaloassignment/src/context/routes-context.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const RouteContext = createContext();
+
+export const RouteProvider = ({ children }) => {
+  const [routes, setRoutes] = useState([]);
+  const addRoute = (route) => {
+    setRoutes((routes) => [...routes, route]);
+  };
+  return (
+    <RouteContext.Provider value={{ routes, setRoutes, addRoute }}>
+      {children}
+    </RouteContext.Provider>
+  );
+};
+
+export const useRoute = () => useContext(RouteContext);
