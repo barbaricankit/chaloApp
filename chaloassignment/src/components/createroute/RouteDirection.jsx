a214dcb81e8a8048c7128842f4f49fe2468fad2a
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 18:19:11 +0530

    Added Error component to Route and RouteDirection

--- chaloassignment/src/components/createroute/RouteDirection.jsx
+++ chaloassignment/src/components/createroute/RouteDirection.jsx
@@ -1,16 +1,26 @@
 import { useCreateRoute } from "../../context/createroute-context";
+import Error from "../Error";
 
 const RouteDirection = () => {
-    const { createRoute: { route: { direction } }, dispatch } = useCreateRoute();
-    const enterRouteNumber = (e) => {
-        dispatch({ type: "ENTER_ROUTE_NAME", payload: { value: e.target.value } })
-    }
-    return <div>
-        <select value={direction} onChange={enterRouteNumber}>
-            <option value="UP">Up</option>
-            <option value="DOWN">Down</option>
-        </select>
+  const {
+    createRoute: {
+      route: { direction },
+      error,
+    },
+    dispatch,
+  } = useCreateRoute();
+  const enterRouteNumber = (e) => {
+    dispatch({ type: "ENTER_ROUTE_NAME", payload: { value: e.target.value } });
+  };
+  return (
+    <div>
+      <select value={direction} onChange={enterRouteNumber}>
+        <option value="UP">Up</option>
+        <option value="DOWN">Down</option>
+      </select>
+      <Error message={error.routeDirection} />
     </div>
-}
+  );
+};
 
-export default RouteDirection
+export default RouteDirection;
