6530fc7644f1406da42a08ebec14aa034a42e626
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 17:07:28 +0530

    Rendering StopDetails in createRoute page

--- chaloassignment/src/components/createroute/CreateRoute.jsx
+++ chaloassignment/src/components/createroute/CreateRoute.jsx
@@ -2,24 +2,33 @@ import AddStop from "./AddStop";
 import Route from "./Route";
 import RouteDirection from "./RouteDirection";
 import RouteStatus from "./RouteStatus";
-import style from "../../styles/createroute.module.css"
 import NavHeader from "../navheader/NavHeader";
+import StopDetails from "./StopDetails";
+import style from "../../styles/createroute.module.css";
+import { useCreateRoute } from "../../context/createroute-context";
 
 const CreateRoute = () => {
-
-    return <>
-        <NavHeader />
-        <div className={style.create_form}>
-            <div className={style.route_details}>
-                <Route />
-                <div className={style.route_dir_status}>
-                    <RouteDirection />
-                    <RouteStatus />
-                    <AddStop />
-                </div>
-            </div>
+  const {
+    createRoute: { stops },
+  } = useCreateRoute();
+  return (
+    <>
+      <NavHeader />
+      <div className={style.create_form}>
+        <div className={style.route_details}>
+          <Route />
+          <div className={style.route_dir_status}>
+            <RouteDirection />
+            <RouteStatus />
+            <AddStop />
+          </div>
+          {stops.map((stop) => (
+            <StopDetails key={stop.stopId} stop={stop} stopId={stop.stopId} />
+          ))}
         </div>
+      </div>
     </>
-}
+  );
+};
 
-export default CreateRoute
+export default CreateRoute;
