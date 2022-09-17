488e846de7632c0e2a31dba9002b8cf2eb251cdd
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 22:09:40 +0530

    Added link to removeroute on Remove Route button click

--- chaloassignment/src/components/home/RemoveRoute.jsx
+++ chaloassignment/src/components/home/RemoveRoute.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 import style from "../../styles/homepage.module.css";
 
 const RemoveRoute = () => {
-
-    return <button className={`${style.remove_route_btn} border_none m_1 btn_pd br_4`}>Remove A Route</button>
-
-}
-export default RemoveRoute
+  return (
+    <Link to="/removeroute">
+      <button
+        className={`${style.remove_route_btn} border_none m_1 btn_pd br_4`}
+      >
+        Remove Route
+      </button>
+    </Link>
+  );
+};
+export default RemoveRoute;
