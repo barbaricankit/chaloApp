788c74cb2a24fccaca92b3c3f205fc3b9e5ad1fb
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 18:27:57 +0530

    Added viewroutes route

--- chaloassignment/src/App.js
+++ chaloassignment/src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles/colors.css'
-import Home from './components/home/Home';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-import CreateRoute from './components/createroute/CreateRoute';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/colors.css";
+import Home from "./components/home/Home";
+import { Routes, Route } from "react-router-dom";
+import CreateRoute from "./components/createroute/CreateRoute";
+import ViewRoutes from "./components/viewroutes/ViewRoutes";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Home />}/> 
-      <Route path="/createroute" element={<CreateRoute />}/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/createroute" element={<CreateRoute />} />
+        <Route path="/viewroutes" element={<ViewRoutes />} />
       </Routes>
     </div>
   );
