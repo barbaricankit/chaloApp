2dba55b00f4699abdd012d7d7d71d166c78336da
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 11:57:35 +0530

    Rendering RouteDirection and RouteStatus component

--- chaloassignment/src/components/Form/CreateForm.jsx
+++ chaloassignment/src/components/Form/CreateForm.jsx
@@ -1,9 +1,13 @@
 import Route from "./Route";
+import RouteDirection from "./RouteDirection";
+import RouteStatus from "./RouteStatus";
 
 const CreateForm=()=>{
 
     return <div>
         <Route />
+        <RouteDirection />
+        <RouteStatus />
     </div>
 }
 
