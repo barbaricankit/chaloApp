59ae3b731e8c18664d6a767c993deb3ead0c692d
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 15:53:46 +0530

    Added BrowserRouter in index.js

--- chaloassignment/src/index.js
+++ chaloassignment/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+
+} from "react-router-dom";
 import { CreateFormProvider } from './context/createform-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <CreateFormProvider>
     <App />
     </CreateFormProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
