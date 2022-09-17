bd06be39a0e5691a73520bb72d7afbcdd83ec1d1
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 14:50:48 +0530

    Added routes for Homepage and CreateRoute

--- chaloassignment/src/App.js
+++ chaloassignment/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import './styles/colors.css'
 import Home from './components/home/Home';
-
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import CreateRoute from './components/Form/CreateRoute';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+      <Route path="/" element={<Home />}/> 
+      <Route path="/createroute" element={<CreateRoute />}/> 
+      </Routes>
     </div>
   );
 }
