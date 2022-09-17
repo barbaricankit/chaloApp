a8b3563b49790122c36587fc072379778cc9fe18
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 16:16:44 +0530

    Added onclick to AddStops button

--- chaloassignment/src/components/createroute/AddStop.jsx
+++ chaloassignment/src/components/createroute/AddStop.jsx
@@ -1,9 +1,10 @@
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { useCreateRoute } from "../../context/createform-context";
 import style from "../../styles/createroute.module.css";
 
 const AddStop = () => {
-
-    return <div className={style.add_stop_btn} >
+    const { addStops } = useCreateRoute();
+    return <div className={style.add_stop_btn} onClick={addStops}>
         <IoMdAddCircleOutline alt="Add Icon" />
         <span>Add Stops</span>
     </div>
