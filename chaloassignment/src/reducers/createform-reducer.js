357c47711f5e0cb96ab07ed7cbbed38fd81cfb14
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 16:15:13 +0530

    Added case ADD_NEW_STOP in reducer

--- chaloassignment/src/reducers/createform-reducer.js
+++ chaloassignment/src/reducers/createform-reducer.js
@@ -2,7 +2,14 @@ export const manageState=(state,action)=>{
 
     switch(action.type){
         case "ENTER_ROUTE_NAME":
-            return {...state,route:{...state.route,routeName:action.payload.value}}
+            return {...state,route:{...state.route,routeName:action.payload.value}};
+        case "ADD_NEW_STOP":
+            return {...state,stops:[...state.stops,{
+                stopId:"",
+                stopName:"",
+                latitude:"",
+                longitude:""
+            }]}
         default :
         return state
     }
