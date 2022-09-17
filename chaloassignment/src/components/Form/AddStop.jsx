9db6e531752f256a3f1f4a8e0d97a795d549cd50
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 12:14:25 +0530

    Added AddStop component

---
+++ chaloassignment/src/components/Form/AddStop.jsx
@@ -0,0 +1,10 @@
+import {IoMdAddCircleOutline} from "react-icons/io"
+const AddStop=()=>{
+
+    return <div >
+        <IoMdAddCircleOutline alt="Add Icon"/> 
+        <span>Add Stop</span>
+    </div>
+}
+
+export default AddStop
