02ad672a311667d71355776bdce9d25c44f2b45d
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 19:09:21 +0530

    Getting and adding data to indexedDB for routes data in route context

--- chaloassignment/src/context/routes-context.js
+++ chaloassignment/src/context/routes-context.js
@@ -1,12 +1,25 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { routesDB } from "../indexedDB/createDB";
 
 const RouteContext = createContext();
 
 export const RouteProvider = ({ children }) => {
   const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await routesDB();
+      setRoutes(result);
+    })();
+  }, []);
+
   const addRoute = (route, stops) => {
     setRoutes((routes) => [...routes, { ...route, stops }]);
+    (async () => {
+      await routesDB({ value: { ...route, stops }, add: true });
+    })();
   };
+
   return (
     <RouteContext.Provider value={{ routes, setRoutes, addRoute }}>
       {children}
