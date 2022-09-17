4639659953350abefacc25d2cc808e09d669c657
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 19:10:46 +0530

    Added Link to View Routes button

--- chaloassignment/src/components/home/ViewRoute.jsx
+++ chaloassignment/src/components/home/ViewRoute.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
 import style from "../../styles/homepage.module.css";
 
 const ViewRoute = () => {
-
-    return <button className={`${style.view_route_btn} border_none m_1 btn_pd br_4`}>View A Route</button>
-}
-export default ViewRoute
+  return (
+    <Link to="/routes">
+      <button className={`${style.view_route_btn} border_none m_1 btn_pd br_4`}>
+        View Routes
+      </button>
+    </Link>
+  );
+};
+export default ViewRoute;
