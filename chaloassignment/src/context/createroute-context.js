3a9ecb0d20abc144614175a9de5bfea65ab16a60
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 18:17:22 +0530

    Added initial value for error in createroute context

--- chaloassignment/src/context/createroute-context.js
+++ chaloassignment/src/context/createroute-context.js
@@ -1,28 +1,34 @@
 import { createContext, useContext, useReducer } from "react";
 import { manageState } from "../reducers/createroute-reducer";
 
-const CreateRouteContext=createContext();
+const CreateRouteContext = createContext();
 
-export const CreateRouteProvider =({children})=>{
-    const initialState={
-        route:{
-            routeName:"",
-            direction:"",
-            routeId:"",
-            status:"Active",
+export const CreateRouteProvider = ({ children }) => {
+  const initialState = {
+    route: {
+      routeName: "",
+      direction: "",
+      routeId: "",
+      status: "Active",
+    },
+    stops: [],
+    error: {
+      routeName: "",
+      routeDirection: "",
+      stops: [],
+    },
+  };
+  const [createRoute, dispatch] = useReducer(manageState, initialState);
 
-        },
-        stops:[]
-    }
-    const [createRoute,dispatch]=useReducer(manageState,initialState);
+  const addStops = () => {
+    dispatch({ type: "ADD_NEW_STOP" });
+  };
 
-    const addStops=()=>{
-        dispatch({type:"ADD_NEW_STOP"})
-    }
-    
-    return <CreateRouteContext.Provider value={{createRoute,dispatch,addStops}}>
-        {children}
+  return (
+    <CreateRouteContext.Provider value={{ createRoute, dispatch, addStops }}>
+      {children}
     </CreateRouteContext.Provider>
-}
+  );
+};
 
-export const useCreateRoute=()=>useContext(CreateRouteContext);
+export const useCreateRoute = () => useContext(CreateRouteContext);
