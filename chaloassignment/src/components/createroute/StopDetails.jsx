e94d19351a83091cd598cd936e155e0a31edefae
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 17:07:09 +0530

    Added logic in StopDetails Component

--- chaloassignment/src/components/createroute/StopDetails.jsx
+++ chaloassignment/src/components/createroute/StopDetails.jsx
@@ -1,15 +1,54 @@
-const StopDetails = ({ stopId, stopName, latitude, longitude }) => {
+import { useCreateRoute } from "../../context/createroute-context";
+import style from "../../styles/createroute.module.css";
+const StopDetails = ({ stop, stopId }) => {
+  const {
+    createRoute: { stops },
+    dispatch,
+  } = useCreateRoute();
 
-    const enterStopId = (e) => {
-        setStopId()
-    }
-
-    return <div>
-        <input type="text" placeholder="Enter Stop Id" value={stopId} onChange={enterStopId} />
-        <input type="text" placeholder="Enter Stop Name" value={stopName} onChange={enterStopName} />
-        <input type="text" placeholder="Enter Latitude" value={latitude} onChange={enterLatitude} />
-        <input type="text" placeholder="Enter Longitude" value={longitude} onChange={enterLongitude} />
+  const enterStopName = (e) => {
+    dispatch({
+      type: "ENTER_STOP_NAME",
+      payload: { stopId, stopName: e.target.value },
+    });
+  };
+  const enterLatitude = (e) => {
+    dispatch({
+      type: "ENTER_LATITUDE",
+      payload: { stopId, latitude: e.target.value },
+    });
+  };
+  const enterLongitude = (e) => {
+    dispatch({
+      type: "ENTER_LONGITUDE",
+      payload: { stopId, longitude: e.target.value },
+    });
+  };
+  return (
+    <div className={style.stop_details}>
+      <input
+        type="text"
+        placeholder="Stop Name"
+        className={`${style.stop_name} m_0_5`}
+        value={stop.stopName}
+        onChange={enterStopName}
+      />
+      <input
+        type="text"
+        placeholder="Latitude"
+        className={`${style.lat} m_0_5`}
+        value={stop.latitude}
+        onChange={enterLatitude}
+      />
+      <input
+        type="text"
+        placeholder="Longitude"
+        className={`${style.long} m_0_5`}
+        value={stop.longitude}
+        onChange={enterLongitude}
+      />
     </div>
-}
+  );
+};
 
-export default StopDetails
+export default StopDetails;
