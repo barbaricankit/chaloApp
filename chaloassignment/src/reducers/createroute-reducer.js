82d8edb3570166ff889dcd4aed0f21d48f5f4d20
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-17 17:08:43 +0530

    Added new cases in reducer related to StopDetails

--- chaloassignment/src/reducers/createroute-reducer.js
+++ chaloassignment/src/reducers/createroute-reducer.js
@@ -1,16 +1,54 @@
-export const manageState=(state,action)=>{
+import { v4 as uuid } from "uuid";
 
-    switch(action.type){
-        case "ENTER_ROUTE_NAME":
-            return {...state,route:{...state.route,routeName:action.payload.value}};
-        case "ADD_NEW_STOP":
-            return {...state,stops:[...state.stops,{
-                stopId:"",
-                stopName:"",
-                latitude:"",
-                longitude:""
-            }]}
-        default :
-        return state
-    }
-}
+export const manageState = (state, action) => {
+  switch (action.type) {
+    case "ENTER_ROUTE_NAME":
+      return {
+        ...state,
+        route: { ...state.route, routeName: action.payload.value },
+      };
+    case "ADD_NEW_STOP":
+      return {
+        ...state,
+        stops: [
+          ...state.stops,
+          {
+            stopId: uuid(),
+            stopName: "",
+            latitude: "",
+            longitude: "",
+          },
+        ],
+      };
+
+    case "ENTER_STOP_NAME":
+      return {
+        ...state,
+        stops: state.stops.map((stop) =>
+          stop.stopId !== action.payload.stopId
+            ? stop
+            : { ...stop, stopName: action.payload.stopName }
+        ),
+      };
+    case "ENTER_LATITUDE":
+      return {
+        ...state,
+        stops: state.stops.map((stop, index) =>
+          stop.stopId !== action.payload.stopId
+            ? stop
+            : { ...stop, latitude: action.payload.latitude }
+        ),
+      };
+    case "ENTER_LONGITUDE":
+      return {
+        ...state,
+        stops: state.stops.map((stop, index) =>
+          stop.stopId !== action.payload.stopId
+            ? stop
+            : { ...stop, longitude: action.payload.longitude }
+        ),
+      };
+    default:
+      return state;
+  }
+};
