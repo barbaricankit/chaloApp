f9f322a580dc1c3b6c90b66e9f2c67541976f3e5
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 10:18:29 +0530

    Removed EditRoute and RemoveRoute buttons from home page

--- chaloassignment/src/components/home/Home.jsx
+++ chaloassignment/src/components/home/Home.jsx
@@ -1,16 +1,13 @@
-import AddRoute from "./AddRoute"
-import EditRoute from "./EditRoute"
-import RemoveRoute from "./RemoveRoute"
-import ViewRoute from "./ViewRoute"
+import AddRoute from "./AddRoute";
+import ViewRoute from "./ViewRoute";
 import style from "../../styles/homepage.module.css";
 const Home = () => {
-
-    return <div className={style.home_page}>
-        <ViewRoute />
-        <AddRoute />
-        <EditRoute />
-        <RemoveRoute />
+  return (
+    <div className={style.home_page}>
+      <ViewRoute />
+      <AddRoute />
     </div>
-}
+  );
+};
 
-export default Home
+export default Home;
