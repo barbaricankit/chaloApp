29a6cee6e7435a7ebdf8114fab3ff7e455317a89
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 10:14:37 +0530

    Removed RemoveRoute and Edit Route button and components

--- chaloassignment/src/components/home/EditRoute.jsx
+++
@@ -1,7 +0,0 @@
-import style from "../../styles/homepage.module.css";
-
-const EditRoute = () => {
-
-    return <button className={`${style.edit_route_btn} border_none m_1 btn_pd br_4`}>Edit A Route</button>
-}
-export default EditRoute
