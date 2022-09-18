36e94fbd9b2780c1197404aa48382446834fb135
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 10:19:05 +0530

    Added back button to the Nav Header and added some styles

--- chaloassignment/src/components/navheader/NavHeader.jsx
+++ chaloassignment/src/components/navheader/NavHeader.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
+import { BiLeftArrowAlt } from "react-icons/bi";
 import style from "../../styles/navheader.module.css";
 
 const NavHeader = () => {
+  const navigate = useNavigate();
   return (
     <div className={style.nav_header}>
-      <Link to="/">
-        <nav className={style.nav_option}>Home</nav>
+      <span className={style.nav_back_option} onClick={() => navigate(-1)}>
+        <BiLeftArrowAlt />
+      </span>
+      <Link to="/" className={style.nav_option}>
+        <nav>Home</nav>
       </Link>
       <div className={style.app}>
         <p className={style.app_name}>Chalo</p>
