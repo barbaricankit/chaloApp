eda2984556d574f79c3be8e3e768e386c89ca8bd
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 16:04:19 +0530

    Added image and nav header in home screen

--- chaloassignment/src/components/home/Home.jsx
+++ chaloassignment/src/components/home/Home.jsx
@@ -1,12 +1,21 @@
 import AddRoute from "./AddRoute";
 import ViewRoute from "./ViewRoute";
 import style from "../../styles/homepage.module.css";
+import NavHeader from "../navheader/NavHeader";
 const Home = () => {
   return (
-    <div className={style.home_page}>
-      <ViewRoute />
-      <AddRoute />
-    </div>
+    <>
+      <NavHeader home={true} />
+      <div className={style.home_page}>
+        <img
+          src="/icons/home_screen.jpeg"
+          alt="Home screen image"
+          className={style.home_screen_image}
+        />
+        <ViewRoute />
+        <AddRoute />
+      </div>
+    </>
   );
 };
 
