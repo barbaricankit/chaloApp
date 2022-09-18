9b0f6b94d5cb7efa1360083ba59f63e63a51b03e
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 10:20:43 +0530

    Added Edit and Remove Icon to the Route Detail component

--- chaloassignment/src/components/viewroutes/RouteDetail.jsx
+++ chaloassignment/src/components/viewroutes/RouteDetail.jsx
@@ -1,9 +1,26 @@
-const RouteDetail = ({ route }) => {
+import RemoveIcon from "../removeroute/RemoveIcon";
+import EditIcon from "../editroute/EditIcon";
+import style from "../../styles/routes.module.css";
+import { Link } from "react-router-dom";
+
+const RouteDetail = ({ route, index }) => {
   return (
-    <div>
-      <p>{route.routeName}</p>
-      <p>{route.direction}</p>
-      <p>{route.status}</p>
+    <div
+      className={`${style.route_detail} ${
+        index % 2 == 0 ? style.even_list_data : style.odd_list_data
+      }`}
+    >
+      <Link to={`/route/${route.routeId}`} className={style.route_link}>
+        <div className={style.route_info}>
+          <p>{route.routeName}</p>
+          <p>{route.direction}</p>
+          <p className={style.route_status}>{route.status}</p>
+        </div>
+      </Link>
+      <div className={style.edit_remove_btns}>
+        <EditIcon route={route} />
+        <RemoveIcon route={route} />
+      </div>
     </div>
   );
 };
