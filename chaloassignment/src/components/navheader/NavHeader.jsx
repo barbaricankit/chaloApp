84ca13829f1ae74028c7cdfde3e2d05c1cef0353
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 16:04:50 +0530

    Added check for NavHeader to display options acc to page

--- chaloassignment/src/components/navheader/NavHeader.jsx
+++ chaloassignment/src/components/navheader/NavHeader.jsx
@@ -3,18 +3,23 @@ import { useNavigate } from "react-router";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import style from "../../styles/navheader.module.css";
 
-const NavHeader = () => {
+const NavHeader = ({ home }) => {
   const navigate = useNavigate();
   return (
     <div className={style.nav_header}>
-      <span className={style.nav_back_option} onClick={() => navigate(-1)}>
-        <BiLeftArrowAlt />
-      </span>
-      <Link to="/" className={style.nav_option}>
-        <nav>Home</nav>
-      </Link>
+      {!home && (
+        <span className={style.nav_back_option} onClick={() => navigate(-1)}>
+          <BiLeftArrowAlt />
+        </span>
+      )}
+      {!home && (
+        <Link to="/" className={style.nav_option}>
+          <nav>Home</nav>
+        </Link>
+      )}
+      {home && <nav className={style.selected_nav}>Home</nav>}
       <div className={style.app}>
-        <p className={style.app_name}>Chalo</p>
+        <p className={style.app_name}>CHALO</p>
       </div>
     </div>
   );
