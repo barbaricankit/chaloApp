f274b980c8f8cb5d0e34b17644a7418234325d83
Ankit Singhania <ankit.singhania@Ankit-Singhania-MAC.local>
2022-09-18 10:20:05 +0530

    Added Search functionality to ViewRoutes Component

--- chaloassignment/src/components/viewroutes/ViewRoutes.jsx
+++ chaloassignment/src/components/viewroutes/ViewRoutes.jsx
@@ -1,16 +1,36 @@
 import RouteDetail from "./RouteDetail";
 import NavHeader from "../navheader/NavHeader";
 import style from "../../styles/routes.module.css";
+import SearchBus from "./SearchBus";
 import { useRoute } from "../../context/routes-context";
 
 const ViewRoutes = () => {
-  const { routes } = useRoute();
+  const { filteredBuses } = useRoute();
   return (
     <>
       <NavHeader />
       <div className={style.routes}>
-        {routes.map((route) => (
-          <RouteDetail route={route} />
+        <SearchBus />
+
+        <div className={style.routes_list_header}>
+          <div className={style.routes_list_detail_header}>
+            <p>Bus Number</p>
+            <p>Direction</p>
+            <p>Status</p>
+          </div>
+          <div className={style.routes_list_btn_header}>
+            <p className={style.edit_btn}>Edit</p>
+            <p className={style.edit_btn}>Remove</p>
+          </div>
+        </div>
+
+        {filteredBuses.map((route, index) => (
+          <RouteDetail
+            key={route.routeId}
+            route={route}
+            remove={true}
+            index={index}
+          />
         ))}
       </div>
     </>
